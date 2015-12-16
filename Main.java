083176d45582fa960fa4a38416455722c22787ca
Bradley Dufour <LordMilton@users.noreply.github.com>
2015-12-16 08:39:41 -0500

    Did this problem.

--- Main.java
+++ Main.java
@@ -1,10 +1,17 @@
-//TODO: <name>
-//TODO: <date>
+//Bradley Dufour
+//2015-12-16
 //Practice 2011 - 00: Hello Judge
+import java.util.Scanner;
 
-public class  {
-  public static void main(String[] args) {
-
-    System.exit(0);
-  }
+public class Main {
+   public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      
+      int judges = scan.nextInt();
+      for(int i = 1; i <= judges; i++)
+      {
+         System.out.println("Hello World, Judge "+ i +"!");
+      }
+      System.exit(0);
+   }
 }
